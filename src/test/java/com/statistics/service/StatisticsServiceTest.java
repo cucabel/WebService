ec2dfbb01bfc774803ca3eef9b1cbbc5df023a41
cucabel <46871266+cucabel@users.noreply.github.com>
2021-06-11 13:26:39 +0200

    mockito

--- src/test/java/com/statistics/service/StatisticsServiceTest.java
+++ src/test/java/com/statistics/service/StatisticsServiceTest.java
@@ -1,44 +1,64 @@
 package com.statistics.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.statistics.domain.Statistic;
 import com.statistics.domain.Transaction;
 import com.statistics.repository.TransactionRepository;
+import com.statistics.service.impl.StatisticServiceImpl;
 
 @SpringBootTest
 public class StatisticsServiceTest {
 
-	@Autowired
-	StatisticService statisticService;
-	@Autowired
+	@InjectMocks
+	StatisticServiceImpl statisticServiceImpl;
+	
+	@Mock
 	TransactionRepository transactionRepository;
+	
+	String currentTimestamp;
+	List<Transaction> transactions = new ArrayList<>();
+	Statistic statistic;
+	
+	@BeforeEach
+	public void init() {
+		
+		MockitoAnnotations.initMocks(this);
 
-	@Test
-	public void testGetStatistic() {
-
-		String currentTimestamp = DateTimeFormatter.ISO_INSTANT.format(ZonedDateTime.now());
-		transactionRepository.save(new Transaction("-5000.00", currentTimestamp));
-		transactionRepository.save(new Transaction("400.00", currentTimestamp));
-		transactionRepository.save(new Transaction("6000.00", currentTimestamp));
-
-		Statistic statistic = new Statistic();
+		currentTimestamp = DateTimeFormatter.ISO_INSTANT.format(ZonedDateTime.now());
+		
+		transactions.add(new Transaction("-5000.00", currentTimestamp));
+		transactions.add(new Transaction("400.00", currentTimestamp));
+		transactions.add(new Transaction("6000.00", currentTimestamp));	
+		
+		statistic = new Statistic();
 		statistic.setSum(new BigDecimal("1400.00"));
 		statistic.setAvg(new BigDecimal("466.67"));
 		statistic.setMax(new BigDecimal("6000.00"));
 		statistic.setMin(new BigDecimal("-5000.00"));
 		statistic.setCount((long) 3);
+	}
 
-		assertEquals(statistic, statisticService.getStatistic());
-
+	@Test
+	public void testGetStatistic() {
+		
+		when(transactionRepository.getAll()).thenReturn(transactions);
+		
+		assertEquals(statistic, statisticServiceImpl.getStatistic());
 	}
 
 }
