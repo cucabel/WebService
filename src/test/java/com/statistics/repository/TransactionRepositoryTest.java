aecf17abf19e10106a02be0586d21770eee24429
cucabel <i.m.martinez@hotmail.es>
2021-06-16 14:21:37 +0200

    update route

--- src/test/java/com/statistics/repository/TransactionRepositoryTest.java
+++ src/test/java/com/statistics/repository/TransactionRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.statistic.repository;
+package com.statistics.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
